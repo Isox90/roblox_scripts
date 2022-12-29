6b5fa526058a270005a25b790eebc820e48d4a3d
Isox90 <step1kon@gmail.com>
2022-12-29 02:01:42 +0100

    Add files via upload

---
+++ autoexec/big.lua
@@ -0,0 +1,7 @@
+getgenv().AuthKey="HUGE_PHS3MhnITrEtND9A"
+getgenv().LoadSettings={
+    UseSeperateSaves=false,
+    DisableStartupPrompt=true,
+    DisableGamepassCheck=false,
+}
+loadstring(game:HttpGet("https://hugegames.io/script"))()
